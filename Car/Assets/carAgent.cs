a5105e25d80840ee7460df7bd5f40fa5962011cc
Mads <madhan16@student.aau.dk>
2019-10-31 13:40:41 +0100

    31-10

---
+++ Car/Assets/carAgent.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+public class carAgent : Agent
+{
+    Rigidbody rBody;
+    public WheelCollider front_driver_col, front_passenger_col;
+    public WheelCollider back_driver_col, back_passenger_col;
+    public Transform frontDriver, frontPassenger;
+    public Transform backDriver, backPassenger;
+    float _steerAngle = 30.0f;
+    float _motorForce = 500f;
+    float _steerangl = 0.0f;
+
+    internal Vector3 startPos;
+    internal Vector3 startRot;
+
+
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+        //startPos = transform.position;
+        //startRot = transform.eulerAngles;
+        back_driver_col.motorTorque = _motorForce;
+        back_passenger_col.motorTorque = _motorForce;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        updateWheelPos(front_driver_col, frontDriver);
+        updateWheelPos(front_passenger_col, frontPassenger);
+        updateWheelPos(back_driver_col, backDriver);
+        updateWheelPos(back_passenger_col, backPassenger);
+    }
+
+    public override void AgentReset(){
+        this.rBody.angularVelocity = Vector3.zero;
+        this.rBody.velocity = Vector3.zero;
+        //transform.position = startPos;
+        //transform.eulerAngles = startRot;
+        this.transform.position = new Vector3(5.133f, 0.14f, 16.752f);
+        this.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, 0.0f));
+    }
+
+    public override void CollectObservations(){
+        //position of the agent used for rewards
+        AddVectorObs(this.transform.position);
+
+        //wheelcolliders used to driver and steer the agent
+        //AddVectorObs(back_driver_col.motorTorque);
+        //AddVectorObs(back_passenger_col.motorTorque);
+        AddVectorObs(front_driver_col.steerAngle);
+        AddVectorObs(front_passenger_col.steerAngle);
+    }
+
+
+    public void turnCar(float[] act){
+        var action = Mathf.FloorToInt(act[0]);
+
+        switch(action){
+            case 1:
+                _steerangl = -30.0f;
+                front_driver_col.steerAngle = _steerangl;
+                front_passenger_col.steerAngle = _steerangl;
+                break;
+
+            case 2:
+                _steerangl = 30.0f;
+                front_driver_col.steerAngle = _steerangl;
+                front_passenger_col.steerAngle = _steerangl;
+                break;
+
+            case 3:
+                _steerangl = 0.0f;
+                front_driver_col.steerAngle = _steerangl;
+                front_passenger_col.steerAngle = _steerangl;
+                break;
+        }
+
+    }
+    public override void AgentAction(float[] vectorAction, string textAction){
+        turnCar(vectorAction);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*
+        //Actions, size = 2
+        Vector3 controlSignal = Vector3.zero;
+        controlSignal.x = vectorAction[0];
+       //controlSignal.z = vectorAction[1];
+        //back_driver_col.motorTorque = (controlSignal.z * _motorForce);
+        //back_passenger_col.motorTorque = (controlSignal.z * _motorForce);
+        front_driver_col.steerAngle = (controlSignal.x * _steerAngle);
+        front_passenger_col.steerAngle = (controlSignal.x * _steerAngle);
+
+        //rewards
+        if(controlSignal.z <= 0 || controlSignal.z <= 0){
+            SetReward(-0.5f);
+        }
+        else if (controlSignal.z > 0 || controlSignal.z > 0){
+            SetReward(0.1f);
+        }
+        */
+        if(this.transform.position.y < 0)
+        {
+            SetReward(-100.0f);
+            Done();
+        }
+
+        SetReward(0.005f);
+
+    }
+        private void OnTriggerEnter(Collider collider){
+        if(collider.CompareTag("target")){
+            SetReward(100.0f);
+            Done();
+        }
+
+        if(collider.CompareTag("back")){
+            SetReward(-100.0f);
+            Done();
+        }
+        
+    }
+
+
+        void updateWheelPos(WheelCollider col, Transform t)
+    {
+        Vector3 pos = t.position;
+        Quaternion rot = t.rotation;
+
+        col.GetWorldPose(out pos, out rot);
+        t.position = pos;
+        rot = rot * Quaternion.Euler(new Vector3(0, 0, 90));
+        t.rotation = rot;
+
+    }
+}
--- Car/Assets/test.cs
+++ Car/Assets/test.cs
@@ -2,18 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test : MonoBehaviour
-{
-    
-    // Start is called before the first frame update
-public Vector2 textureSize;
-private Material wallMaterial;
+public class test : MonoBehaviour {
 
-// Update is called once per frame
-void Update() //or Start, if you don't need realtime rescaling.
-{
-    GetComponent<Renderer>().materials[0].mainTextureScale = new Vector2(transform.localScale.x / textureSize.x, transform.localScale.y / textureSize.y);
-    GetComponent<Renderer>().materials[1].mainTextureScale = new Vector2(transform.localScale.x / textureSize.x, transform.localScale.z / textureSize.y);
-    GetComponent<Renderer>().materials[2].mainTextureScale = new Vector2(transform.localScale.z / textureSize.x, transform.localScale.y / textureSize.y);
-}
+    public Rigidbody car;
+    public WheelCollider fl,fr,hl,hr;
+    public float motorMax = 300, steerMax = 60;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	void Update () {
+        float motor = - Input.GetAxis("Vertical");
+        float steer = Input.GetAxis("Horizontal") * steerMax;
+        hl.motorTorque = motor * motorMax;
+        hr.motorTorque = motor * motorMax;
+        Vector3 position;
+        Quaternion rotation;
+        
+        fl.steerAngle = steer;
+        fr.steerAngle = steer;
+        fl.GetWorldPose(out position, out rotation);
+        fl.transform.rotation = rotation;
+        fr.transform.rotation = rotation;
+        hr.GetWorldPose(out position, out rotation);
+        hl.transform.rotation = rotation;
+        hr.transform.rotation = rotation;
+        Debug.Log(car.velocity);
+    }
 }
