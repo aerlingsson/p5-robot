94a02a4f494c461537c38443b6b79b847ebb951d
aerlin17 <aerlin17@student.aau.dk>
2019-11-12 13:33:16 +0100

    update 2 of merging agent and bt conn

--- app/src/main/java/com/example/p5_robot/Agent/CameraPageActivity.java
+++ app/src/main/java/com/example/p5_robot/Agent/CameraPageActivity.java
@@ -209,7 +209,7 @@ public class CameraPageActivity extends AppCompatActivity {
         super.onResume();
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_11, this, mLoaderCallback);
         } else {
             Log.d(TAG, "OpenCV library found inside package. Using it!");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
