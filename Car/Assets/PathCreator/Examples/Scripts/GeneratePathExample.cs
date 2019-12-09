4986e2973ccf35af0e2a72b6815243351481a145
Mads <madhan16@student.aau.dk>
2019-12-09 14:41:23 +0100

    changed height of road

--- Car/Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
+++ Car/Assets/PathCreator/Examples/Scripts/GeneratePathExample.cs
@@ -6,12 +6,23 @@ namespace PathCreation.Examples {
     [RequireComponent(typeof(PathCreator))]
     public class GeneratePathExample : MonoBehaviour {
 
-        public bool closedLoop = true;
+        public bool closedLoop = false;
         public Transform[] waypoints;
 
         void Start () {
             if (waypoints.Length > 0) {
                 // Create a new bezier path from the waypoints.
+                foreach (Transform t in waypoints)
+                {
+                    float zRange = Random.Range (0.0f, 3.0f);
+                    int front = Random.Range(0, 2);
+                    if(front == 0){
+                    t.position = t.position + new Vector3(0,0,zRange);
+                    }else
+                    {
+                        t.position = t.position - new Vector3(0,0,zRange);
+                    }
+                }
                 BezierPath bezierPath = new BezierPath (waypoints, closedLoop, PathSpace.xyz);
                 GetComponent<PathCreator> ().bezierPath = bezierPath;
             }
