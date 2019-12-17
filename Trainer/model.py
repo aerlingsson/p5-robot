1ee734c85ec1447323528cab37bea4d8157acd9e
RasmusHRAAU <34272322+RasmusHRAAU@users.noreply.github.com>
2019-12-17 15:31:08 +0100

    Update model.py
    Delete outcommented model

--- Trainer/model.py
+++ Trainer/model.py
@@ -4,26 +4,6 @@ import tensorflow as tf
 
 
 def build_model(state_shape, action_size):
-    '''
-            encoder = load_model('enc.h5')
-            encoder.trainable = False
-
-            inp = Input(shape=self.state_shape)
-            lamb1 = Lambda(lambda x: tf.image.rgb_to_grayscale(x))(inp)
-            lamb2 = Lambda(lambda x: x / 255)(lamb1)
-            enc = encoder(lamb2)
-            flatten = Flatten()(enc)
-            drp1 = Dropout(0.3)(flatten)
-            dense1 = Dense(100, activation='relu')(drp1)
-            drp2 = Dropout(0.3)(dense1)
-            dense2 = Dense(50, activation='relu')(drp2)
-            drp3 = Dropout(0.2)(dense2)
-            dense3 = Dense(self.action_size, activation='linear')(drp3)
-
-            model = Model(inputs=inp, outputs=dense3)
-
-            '''
-
     model = Sequential([
         Input(shape=state_shape),
         Lambda(lambda x: tf.image.rgb_to_grayscale(x)),
@@ -33,11 +13,7 @@ def build_model(state_shape, action_size):
         Conv2D(48, (5, 5), strides=(2, 2), activation='relu'),
         Conv2D(64, (3, 3), strides=(1, 1), activation='relu'),
         Flatten(),
-        Dropout(0.3),
-        Dense(100, activation='relu'),
-        Dropout(0.3),
-        Dense(50, activation='relu'),
-        Dropout(0.2),
+        Dense(256, activation='relu'),
         Dense(action_size, activation='linear')
     ])
 
